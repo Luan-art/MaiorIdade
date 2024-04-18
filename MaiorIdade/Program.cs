d979517a3ecf8bce5694186ea89d015964b2c128
Luan-art <luanlf2003@gmail.com>
2024-04-18 16:01:43 -0300

    Add project files.

---
+++ MaiorIdade/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade;
+
+Console.WriteLine("Digita sua idade");
+
+idade = int.Parse(Console.ReadLine());
+
+if(idade < 0)
+{
+
+    Console.WriteLine("Invalido");
+
+} else if (idade >= 18)
+{
+    Console.WriteLine("Maior de idade");
+
+}
+else
+{
+    Console.WriteLine("Menor de idade");
+    Console.WriteLine();
+}
